02f818df123ebeaec13c64e3b71b026de6aef773
Farooq <syed@farooq.dev>
2022-06-08 20:42:48 -0500

    Added an api call and blocked user list.

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ import os
 import re 
 from datetime import datetime, time, timedelta
 from dotenv import load_dotenv
+import requests
 
 
 EVENTS = []
 COUNT = 1
 load_dotenv()
 client = discord.Client()
+BLOCKED_IDS = [139981655019487232] #kevin
+ANNOUNCEMENTS = []
 
 def formatChecker(msg):
     # Correct:
     # create Valorant 03/02 09:00 @gello
+
+    if(len(msg) < 4):
+        return "Incorrect format. Type 'event help' to see the correct format."
+
     date = msg[2]
     time = msg[3]
     dateReg = re.search("^(0?[1-9]|1[012])/[1-2][0-9]|30|31|[0][1-9]$", date) # Check if date is valid
     timeReg = re.search("^(0[1-9]|1[0-9]|2[0123]):[0-5][0-9]$", time) # Check if time is valid
-
-    if(len(msg) < 4):
-        return "Incorrect format. Type 'event help' to see the correct format."
-    elif not (dateReg):
+    
+    if not (dateReg):
         return "Incorrect date format. Type 'event help' to see the correct format."
-    elif not (timeReg):
+    if not (timeReg):
         return "Incorrect time format. Type 'event help' to see the correct format."
     else:
         return ""
@@ -43,6 +48,12 @@ def newEvent(msg, author):
     increment()
     print(EVENTS)
 
+def blockedUsers(message):
+    return "Go f yourself"
+
+def getInsults():
+    return requests.get("https://insult.mattbas.org/api/insult").text
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -51,14 +62,26 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content.startswith('insult'):
+        userMessage = message.content.split(" ")
+        if "<@130824291787735040>" in userMessage:
+            return await message.channel.send("Can't insult Daddy 😘")
+
+        usersInsulted = ""
+        if(len(userMessage) > 1):
+            for x in userMessage[1:]:
+                usersInsulted += x + " "
+            
+        await message.channel.send(usersInsulted.strip() + "  " + getInsults())
 
     # date_time_str = datetime.now().strftime("%m/%d %H:%M")
-
-    # await message.channel.send(message.author)
-
+    
     if message.content.startswith('create'):
+        if message.author.id in BLOCKED_IDS:
+            return await message.channel.send(blockedUsers(message))
+
         userMessage = message.content.split(" ")
-        print(userMessage)
         wrongFormat = formatChecker(userMessage)
         if(wrongFormat):
             await message.channel.send(wrongFormat)
@@ -67,6 +90,8 @@ async def on_message(message):
             await message.channel.send("Event added! Type 'events' to list all the events")
 
     if message.content.startswith('events'):
+        if message.author.id in BLOCKED_IDS:
+            return await message.channel.send(blockedUsers(message))
         if(len(EVENTS) == 0):
             await message.channel.send("No events")
         else:
@@ -76,7 +101,13 @@ async def on_message(message):
                 await message.channel.send(str(x))
     
     if message.content.startswith('event help'):
+        if message.author.id in BLOCKED_IDS:
+            return await message.channel.send(blockedUsers(message))
         await message.channel.send("To create an event, please use this format:\n `create CSGO 06/08 09:00 @csnerd`")
-        
+
+    if message.author not in ANNOUNCEMENTS:
+        ANNOUNCEMENTS.append(message.author)
+        await message.channel.send("Do you have $1? <@"+str(message.author.id)+"> Buy this shit dumbass \n https://www.humblebundle.com/games/capcom-summer-2022-bundle")
+
 
 client.run(os.getenv('token'))
